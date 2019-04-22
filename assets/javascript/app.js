9a1fd2fe651600bb952f540df39dc13d200fe85a
Jennifer Grace <jennifergrace@Jennifers-MacBook-Pro.local>
2019-04-21 20:12:27 -0400

    timer is working

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -4,21 +4,22 @@
  * JavaScript file for Harry Potter Trivia
  ***********************************************/
 
+
 $(document).ready(function () {
 
     var questionOne = {
-        "question": "question array",
-        "answerOne": "answer array",
-        "answerTwo": "answer array2",
-        "answerThree": "answer array3",
-        "answerFour:": "answer array4",
-        "correctAnswer": "answer correct one"
+        "question": "What house at Hogwarts does Harry belong to?",
+        "answerOne": "Hufflepuff",
+        "answerTwo": "Gryffindor",
+        "answerThree": "Ravenclaw",
+        "answerFour": "Slytherin",
+        "correctAnswer": "Gryffindor"
     }
 
     // jQuery variables
-    var answersSpaceDiv = $("#answers-space");
-    var questionSpaceDiv = $("#question-space");
-    var questonAnswerSpaceDive = $("#question-answer-space");
+
+    var questionAnswerSpaceDiv = $("#question-answer-space");
+    var startDiv = $("#start-button");
 
     // variables and arrays
     var questionsArray = [questionOne];
@@ -27,15 +28,31 @@ $(document).ready(function () {
     var secondAnswer;
     var thirdAnswer;
     var fourthAnswer;
-    var correctAnswer;
+    // var correctAnswer;
+    var time = 15;
 
-    console.log("here")
-    buildQuestions(questionsArray);
+    //  This code will run as soon as the page loads.
+    window.onload = function () {
+        startDiv.on("click", startGame);
+    };
+
+
+    // start game 
+    function startGame() {
+        // clear start button out of div
+        questionAnswerSpaceDiv.empty();
+
+        var intervalId = setInterval(count, 1000)
+        buildQuestions(questionsArray);
+        setTimeout(function () {
+            timeUp(intervalId);
+        }, 15000);
+    }
+
 
     // build questions
     function buildQuestions(questionsArray) {
         $.each(questionsArray, function () {
-            console.log("this" + this);
             var thisQuestion = this;
             question = thisQuestion.question;
             firstAnswer = thisQuestion.answerOne;
@@ -43,19 +60,59 @@ $(document).ready(function () {
             thirdAnswer = thisQuestion.answerThree;
             fourthAnswer = thisQuestion.answerFour;
             thisCorrectAsnwer = thisQuestion.correctAnswer;
-            console.log("question " + thisQuestion.question)
-            var questionDiv = $("<div/>", { id: "question", text: question });
-            var answerOneDiv = $("<div/>", { id: "answer-one", text: firstAnswer });
-            var answerTwoDiv = $("<div/>", { id: "answer-two", text: secondAnswer });
-            var answerThreeDiv = $("<div/>", { id: "answer-three", text: thirdAnswer });
-            var answerFourDiv = $("<div/>", { id: "answer-four", text: fourthAnswer });
+            questionSpaceDiv = $("<div>", { class: "question-space" });
+            var questionDiv = $("<h1>", { id: "question", text: question });
+            var answersDiv = $("<ol>");
+            answerSpaceDiv = $("<div>", { class: "answer-space" });
+            var answerOneDiv = $("<li>", { id: "answer-one", text: firstAnswer });
+            var answerTwoDiv = $("<li>", { id: "answer-two", text: secondAnswer });
+            var answerThreeDiv = $("<li>", { id: "answer-three", text: thirdAnswer });
+            var answerFourDiv = $("<li>", { id: "answer-four", text: fourthAnswer });
 
-            questonAnswerSpaceDive.append(questionDiv);
-            questonAnswerSpaceDive.append(answerOneDiv);
-            questonAnswerSpaceDive.append(answerTwoDiv);
-            questonAnswerSpaceDive.append(answerThreeDiv);
-            questonAnswerSpaceDive.append(answerFourDiv);
-
+            questionSpaceDiv.append(questionDiv);
+            questionAnswerSpaceDiv.append(questionSpaceDiv);
+            answersDiv.append(answerOneDiv);
+            answersDiv.append(answerTwoDiv);
+            answersDiv.append(answerThreeDiv);
+            answersDiv.append(answerFourDiv);
+            answerSpaceDiv.append(answersDiv);
+            questionAnswerSpaceDiv.append(answerSpaceDiv);
         })
+    }
+
+    function count() {
+
+        time--
+
+        currentTime = timeConverter(time);
+        var timerDiv = $("<div>", { id: "timer", html: "Time Left: " + currentTime });
+        $("#timer-col").html(timerDiv);
+    }
+
+
+    function timeConverter(t) {
+
+        var minutes = Math.floor(t / 60);
+        var seconds = t - (minutes * 60);
+
+        if (seconds < 10) {
+            seconds = "0" + seconds;
+        }
+
+        if (minutes === 0) {
+            minutes = "00";
+        }
+
+        else if (minutes < 10) {
+            minutes = "0" + minutes;
+        }
+
+        return minutes + ":" + seconds;
+    }
+
+    function timeUp(intervalId) {
+        $("#timer-col").empty();
+        clearInterval(intervalId);
+        questionAnswerSpaceDiv.html("<h2>Time's Up</h2>");
     }
 })
